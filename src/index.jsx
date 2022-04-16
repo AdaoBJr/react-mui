5523b334e45d66389b7f5dfab2e0f4cbdb83ee9b
AdaoBJr <adaobjr@gmail.com>
2022-04-15 22:13:51 -0400

    feat: :sparkles: updated config on index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,12 @@
-import { render } from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
