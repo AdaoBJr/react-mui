6ab4820b1a1e0b14a80e6a6eeea1adde159a767a
AdaoBJr <adaobjr@gmail.com>
2022-04-16 10:25:49 -0400

    feat: :art: updated Home Comp

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './home.css';
 
 const home = () => {
-  return <div className={styles.root}>Home</div>;
+  const [increment, setIncrement] = useState(0);
+  return (
+    <>
+      {increment}
+      <button
+        type="button"
+        className={styles.root}
+        onClick={() => setIncrement(increment + 1)}
+      >
+        Increment
+      </button>
+    </>
+  );
 };
 
 export default home;
