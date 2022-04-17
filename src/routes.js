3178764a12020a00e55c8d071841e4d071fd5ec8
AdaoBJr <adaobjr@gmail.com>
2022-04-17 14:50:09 -0400

    feat: :fire: removed imported not used

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './lib/components/Home';
-import CreateAccount from './lib/components/CreateAccount';
 
 const Router = () => {
   return (
