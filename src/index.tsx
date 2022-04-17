4dc3d1c73337f0406f01a2a2fa6cbc9ddc9fc93f
AdaoBJr <adaobjr@gmail.com>
2022-04-17 14:24:18 -0400

    refactor:

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import { AppThemeProvider } from './contexts/AppThemeContext';
 import './index.css';
+
+import App from './App';
+import { AppThemeProvider, DrawerProvider } from './contexts';
 import SideNavigation from './lib/components/SideNavigation';
 
 const container = document.getElementById('root') as HTMLElement;
@@ -12,11 +13,13 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <AppThemeProvider>
-      <SideNavigation>
-        <Router>
-          <App />
-        </Router>
-      </SideNavigation>
+      <DrawerProvider>
+        <SideNavigation>
+          <Router>
+            <App />
+          </Router>
+        </SideNavigation>
+      </DrawerProvider>
     </AppThemeProvider>
   </React.StrictMode>
 );
