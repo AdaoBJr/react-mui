502688ea803d049796b4117a5381fec57992c078
AdaoBJr <adaobjr@gmail.com>
2022-04-17 08:58:20 -0400

    style: :lipstick: created theme on MUIv5

--- src/components/CreateAccount/createAccount.tsx
+++ src/components/CreateAccount/createAccount.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { Button } from '@mui/material';
 
-const createAccount = () => {
-  return <div>createAccount</div>;
+const createAccount: React.FC = () => {
+  return (
+    <>
+      <div>createAccount</div>
+      <Button variant="contained" color="primary">
+        Registre-se
+      </Button>
+    </>
+  );
 };
 
 export default createAccount;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { LightTheme } from './theme';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={LightTheme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
