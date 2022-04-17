319bcdb25a8189d0ddcc100a9310e6197125ecfe
AdaoBJr <adaobjr@gmail.com>
2022-04-17 11:47:46 -0400

    feat: :art: added Box & spacing theme on sideNavigation

--- src/lib/components/SideNavigation/sideNavigation.tsx
+++ src/lib/components/SideNavigation/sideNavigation.tsx
@@ -1,8 +1,21 @@
-import { Drawer } from '@mui/material';
+import { Drawer, useTheme } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
+import { ReactComponent } from '../../../typesDefault';
 
-const sideNavigation: React.FC = () => {
-  return <Drawer variant="permanent">teste</Drawer>;
+const sideNavigation: React.FC<ReactComponent> = ({ children }) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer variant="permanent">
+        <Box width={theme.spacing(28)}>Dados</Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
 };
 
 export default sideNavigation;
