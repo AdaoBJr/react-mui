f0b8219f88809024de85b8a766d97f2540c18309
AdaoBJr <adaobjr@gmail.com>
2022-04-17 11:31:14 -0400

    feat: :sparkles: created sideNavigation Component

---
+++ src/lib/components/SideNavigation/sideNavigation.tsx
@@ -0,0 +1,8 @@
+import { Drawer } from '@mui/material';
+import React from 'react';
+
+const sideNavigation: React.FC = () => {
+  return <Drawer variant="permanent">teste</Drawer>;
+};
+
+export default sideNavigation;
